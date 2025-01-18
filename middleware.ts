392863ffd50aaa225b7e11781f239049f60d34c7
Antonio Maritato <antonio.maritato@dacadoo.com>
2025-01-18 06:16:21 +0100

    refactor middleware configuration and implement row-level security policies in database

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,7 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: [
 
-    "/((?!_next/static|_next/image|favicon.ico|api/|$|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-    "/dashboard/:path*", // Explicitly include /dashboard/* paths
+    // "/((?!_next/static|_next/image|favicon.ico|api/|$|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    // "/dashboard/:path*",
   ],
 };
--- prisma/scripts.sql
+++ prisma/scripts.sql
@@ -10,3 +10,28 @@ GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO PUBLIC;
 -- Ensure future tables and functions inherit these permissions automatically
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO PUBLIC;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT EXECUTE ON FUNCTIONS TO PUBLIC;
+
+-------------------------RLS -------------------------
+
+ALTER TABLE tenants ENABLE ROW LEVEL SECURITY;
+ALTER TABLE users ENABLE ROW LEVEL SECURITY;
+ALTER TABLE users_info ENABLE ROW LEVEL SECURITY;
+ALTER TABLE tenant_info ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "tenant_owner_only" ON tenants
+FOR SELECT
+USING ("ownerId" = auth.uid());
+
+CREATE POLICY "users_in_tenant" ON users
+FOR ALL
+USING ("tenantId" IN (SELECT "id" FROM tenants WHERE "ownerId" = auth.uid()))
+WITH CHECK ("tenantId" IN (SELECT "id" FROM tenants WHERE "ownerId" = auth.uid()));
+
+CREATE POLICY "users_info_in_tenant" ON users_info
+FOR ALL
+USING ("tenantId" IN (SELECT "id" FROM tenants WHERE "ownerId" = auth.uid()))
+WITH CHECK ("tenantId" IN (SELECT "id" FROM tenants WHERE "ownerId" = auth.uid()));
+
+CREATE POLICY "no_access" ON tenant_info
+FOR ALL
+USING (false);
